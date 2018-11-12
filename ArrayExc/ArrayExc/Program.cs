e37745e8acdef7a776dd2cff3fd62d85a48a146f
Ben Gat <Bengat167@gmail.com>
2018-11-12 13:28:20 +0200

    question 3 added

--- ArrayExc/ArrayExc/Program.cs
+++ ArrayExc/ArrayExc/Program.cs
@@ -14,6 +14,34 @@ namespace ArrayExc
             int[] array= { 1, 2, 3, 1};
             Console.WriteLine(WhatsMyMaxNum(array)+" is the biggest number in the array.");
             Console.WriteLine(MaxEarningsCompany(array,companies)+" is the highest earning company.");
+            int[] filterThisArray = new int []{ 1, 2, 3, 4, 5, 6,7,8,9,10,11,12 };
+            int[] filteredArray =FilterEvanNumbers(filterThisArray);
+            for(int i = 0; i < filteredArray.Length; i++)
+            {
+                Console.Write(filteredArray[i]+" ");
+            }
+            Console.WriteLine();
+        }
+        static int[] FilterEvanNumbers(int[] array)
+        {
+            int count = 0;
+            for(int i = 0; i < array.Length; i++)
+            {
+                if (array[i] % 2 == 0)
+                {
+                    count++;
+                }
+            }
+            int[] evanNumbersArray = new int[count];
+            for(int i = 0, j = 0; i < array.Length; i++)
+            {
+                if (array[i] % 2 == 0)
+                {
+                    evanNumbersArray[j] = array[i];
+                    j++;
+                }
+            }
+            return evanNumbersArray;
         }
         static int WhatsMyMaxNum(int[] array)
         {
