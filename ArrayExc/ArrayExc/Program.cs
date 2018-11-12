d9b87c45151dd377a664e4e75513928ac9530e2d
Ben Gat <Bengat167@gmail.com>
2018-11-12 11:39:41 +0200

    asd

---
+++ ArrayExc/ArrayExc/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayExc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array= { 1, 2, 3, 4, 5, 6 };
+        }
+    }
+}
